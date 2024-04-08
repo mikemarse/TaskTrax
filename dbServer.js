0606eef47b9cf22a64772241e0bcf500e3c32816
Michael <134131019+mikemarse@users.noreply.github.com>
2024-04-08 01:48:47 -0500

    Created route for creating users

--- dbServer.js
+++ dbServer.js
@@ -3,7 +3,8 @@ const app = express();
 const path = require("path");
 require("dotenv").config();
 
-const mysql = require("mysql")
+const mysql = require("mysql");
+const bcrypt = require("bcrypt");
 
 const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
@@ -20,6 +21,7 @@ const db = mysql.createPool({
 	port: DB_PORT							// default port number
 })
 
+//connecting to server
 db.getConnection((err, connection) => {
 	if (err) throw (err);
 	console.log("DB connected successful: " + connection.threadId);
@@ -37,7 +39,49 @@ app.use('dist', express.static(path.join(__dirname, 'dist'), {
 }));
 
 // Other routes and middleware
+app.use(express.json());
 
+app.post("/createUser", async(req, res) => {
+	const salt = bcrypt.genSalt(11);
+
+	const user = req.body.name;
+	const email = req.body.email;
+	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+	db.getConnection(async (err, connection) => {
+		if (err) throw (err);
+		// Searching for the user. Might change this to email... who knows
+		const sqlSearch = "SELECT * FROM userTable WHERE user = ?";
+		const search_query = mysql.format(sqlSearch, [user]);
+
+		// Inserting the user into the database 
+		const sqlInsert = "INSERT INTO userTable VALUES (0, ?, ?, ?)";
+		const insert_query = mysql.format(sqlInsert, [user, email, hashedPassword]);
+		// ? will be replaced by values
+		// ?? will be replaced by strings
+
+		await connection.query(search_query, async(err, result) => {
+			if (err) throw (err);
+			console.log("------> Search Results");
+			console.log(result.length);
+
+			if (result.length !== 0) {
+				connection.release();
+				console.log("------> User already exists");
+				res.sendStatus(400);
+			}
+			else {
+				await connection.query(insert_query, (err, result) => {
+					connection.release();
+					if (err) throw (err);
+					console.log("------> Created new User");
+					console.log(result.insertId);
+					res.sendStatus(201);
+				})
+			}
+		})
+	})
+})
 
 
 // Start the server
